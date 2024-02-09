05dc35fb686a07d3308f1b280a431acd528e1692
denniztr <denniztr@gmail.com>
2024-02-09 19:42:07 +0300

    auth UI completed, crucial input and button completed. Placeholders for registration and authorization are available

--- app/(site)/components/auth-form.tsx
+++ app/(site)/components/auth-form.tsx
@@ -2,9 +2,11 @@
 
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 import Input from '@/app/components/inputs/input';
 import Button from '@/app/components/button';
+import AuthSocialButton from './auth-social-button';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -71,7 +73,7 @@ export default function AuthForm() {
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant === 'REGISTER' && (
-            <Input id="name" label="Name" register={register} errors={errors} />
+            <Input id="name" label="Name" register={register} errors={errors} disabled={isLoading} />
           )}
           <Input
             id="email"
@@ -79,6 +81,7 @@ export default function AuthForm() {
             type="email"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <Input
             id="password"
@@ -86,11 +89,56 @@ export default function AuthForm() {
             type="password"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <div>
-            <Button>test</Button>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                    absolute 
+                    inset-0 
+                    flex 
+                    items-center"
+            >
+              <div
+                className="
+                        w-full 
+                        border-t 
+                        border-gray-300"
+              />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant ? 'New To Messenger?' : 'Already have an account?'}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === 'LOGIN' ? 'Create an account' : 'Log in'}
+          </div>
+        </div>
       </div>
     </div>
   );
