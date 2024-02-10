804aeb9640575eb657012406dbfeff36d105e793
denniztr <denniztr@gmail.com>
2024-02-10 14:36:29 +0300

    credentials login | ok

--- app/(site)/components/auth-form.tsx
+++ app/(site)/components/auth-form.tsx
@@ -8,6 +8,8 @@ import { BsGithub, BsGoogle } from 'react-icons/bs';
 import Input from '@/app/components/inputs/input';
 import Button from '@/app/components/button';
 import AuthSocialButton from './auth-social-button';
+import toast from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -39,11 +41,22 @@ export default function AuthForm() {
     setIsLoading(true);
 
     if (variant === 'REGISTER') {
-      axios.post('/api/register', data);
+      axios
+        .post('/api/register', data)
+        .catch(() => toast.error('Something went wrong'))
+        .finally(() => setIsLoading(false));
     }
 
     if (variant === 'LOGIN') {
-      // next auth signin
+      signIn('credentials', {
+        ...data,
+        redirect: false,
+      })
+      .then((cb) => {
+        if (cb?.error) toast.error('Invalid Credentials');
+        if (cb?.ok && !cb?.error) toast.success('Logged in!');
+      })
+      .finally(() => setIsLoading(false));
     }
   };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import ToasterContext from './context/toaster-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} font-roboto`}>{children}</body>
+      <body className={`${inter.className} font-roboto`}>
+        <ToasterContext />
+        {children}
+      </body>
     </html>
   );
 }
