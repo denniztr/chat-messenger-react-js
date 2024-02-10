56208c7bf484f0ef40cf08c087dab0f5a48cd2c6
denniztr <denniztr@gmail.com>
2024-02-10 15:58:47 +0300

    route to user page and protected middleware config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import ToasterContext from './context/toaster-context';
+import AuthContext from './context/auth-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} font-roboto`}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
