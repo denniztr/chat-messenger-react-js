e00813154e86a8d2cec2e7cda72e9622b7600cc9
denniztr <denniztr@gmail.com>
2024-02-10 18:18:19 +0300

    sidebar, navigation, layout

---
+++ app/components/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+import DesktopSidebar from './desktop-sidebar';
+import MobileFooter from './mobile-footer';
+
+async function Sidebar({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full">
+      <DesktopSidebar />
+      <MobileFooter />
+      <main className="lg:pl-20 h-full">{children}</main>
+    </div>
+  );
+}
+
+export default Sidebar;
