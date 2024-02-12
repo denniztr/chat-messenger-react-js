7454f56eca998891cbcd5e502ad3e94f1d58d670
denniztr <denniztr@gmail.com>
2024-02-12 17:34:47 +0300

    loading modal completed

--- app/users/components/user-box.tsx
+++ app/users/components/user-box.tsx
@@ -5,6 +5,7 @@ import { useCallback, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { User } from '@prisma/client';
 import Avatar from '@/app/components/avatar';
+import LoadingModal from "@/app/components/loading-modal";
 
 interface UserBoxProps {
   data: User;
@@ -28,46 +29,49 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router]);
 
   return (
-    <div
-      className="
-        w-full
-        relative
-        flex
-        items-center
-        space-x-3
-        bg-white
-        p-3
-        hover:bg-neutral-100
-        rounded-lg
-        transition
-        cursor-pointer
-      "
-      onClick={handleClick}
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div
-            className="
-              flex
-              justify-between
-              items-center
-              mb-1  
+      <>
+          {isLoading && <LoadingModal />}
+        <div
+          className="
+            w-full
+            relative
+            flex
+            items-center
+            space-x-3
+            bg-white
+            p-3
+            hover:bg-neutral-100
+            rounded-lg
+            transition
+            cursor-pointer
           "
-          >
-            <p
-              className="
-                text-sm
-                font-medium
-                text-gray-900
+          onClick={handleClick}
+        >
+          <Avatar user={data} />
+          <div className="min-w-0 flex-1">
+            <div className="focus:outline-none">
+              <div
+                className="
+                  flex
+                  justify-between
+                  items-center
+                  mb-1
               "
-            >
-              {data.name}
-            </p>
+              >
+                <p
+                  className="
+                    text-sm
+                    font-medium
+                    text-gray-900
+                  "
+                >
+                  {data.name}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </>
   );
 };
 
